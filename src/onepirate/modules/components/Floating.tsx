f7ab11251c487fa426ea5ef1da9dec735c1f81cf
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-01 01:17:04 +0000

    .

--- src/onepirate/modules/components/Floating.tsx
+++ src/onepirate/modules/components/Floating.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import WhatsappButton from 'react-whatsapp-button';
 
-const Floating: React.FC = () => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Floating: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
-      <h1>Meu Site React</h1>
-      <WhatsappButton
+      {children}
+      <WhatsappButton         
         countryCode="55"
         animated
         phoneNumber="+5511987654321"
-        message="Olá! Estou te enviando uma mensagem pelo meu site."
-      />
+        message="Olá! Estou te enviando uma mensagem pelo meu site."/>
     </div>
   );
 }
+
 
 export default Floating;
