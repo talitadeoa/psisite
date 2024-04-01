222bfd99253a253d9797ca2b5deba5356a5f041a
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-01 02:05:25 +0000

    .

--- src/onepirate/Home.tsx
+++ src/onepirate/Home.tsx
@@ -6,13 +6,23 @@ import ProductValues from "./modules/views/ProductValues";
 import ProductHowItWorks from "./modules/views/ProductHowItWorks";
 import ProductCTA from "./modules/views/ProductCTA";
 import withRoot from "./modules/withRoot";
+import Carousel from "./modules/views/Carousel"; 
 
 function Index() {
+  const slides = [
+    <div key="1">Slide 1</div>,
+    <div key="2">Slide 2</div>,
+    <div key="3">Slide 3</div>,
+    <div key="4">Slide 3</div>,
+    // Adicione mais slides conforme necessário
+  ];
+
   return (
     <React.Fragment>
       <ProductHero />
       <ProductValues />
       <ProductHowItWorks />
+      <Carousel slides={slides} />
       <ProductCTA />
       <ProductSmokingHero />
       <AppFooter />
---
+++ src/onepirate/modules/views/Carousel.tsx
@@ -0,0 +1,47 @@
+// Carousel.tsx
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface CarouselProps {
+  slides: React.ReactNode[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ slides }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings}>
+      {slides.map((slide, index) => (
+        <div key={index}>{slide}</div>
+      ))}
+    </Slider>
+  );
+};
+
+export default Carousel;
