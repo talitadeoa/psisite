e2e8445d5d1209180fced993f6286e8c3d9e34d6
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-01 01:07:37 +0000

    .

--- src/onepirate/modules/components/Floating.tsx
+++ src/onepirate/modules/components/Floating.tsx
@@ -9,8 +9,8 @@ const Floating: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="App">
       <ReactWhatsappButton
-        countryCode="51"
-        phoneNumber="987654321"
+        countryCode="55"
+        phoneNumber="84998026288"
         animated
         message="Olá, vim pelo seu site e gostaria de saber mais informações sobre seu trabalho como psicóloga"
       />
