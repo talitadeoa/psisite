d28f37b773f3f3cf5e677a1115b6fe7ae063387d
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-03 17:59:49 +0000

    .

--- src/onepirate/modules/views/Carousel.tsx
+++ src/onepirate/modules/views/Carousel.tsx
@@ -8,7 +8,7 @@ const TextCarousel: React.FC = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -18,7 +18,6 @@ const TextCarousel: React.FC = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          autoplay: true,
           dots: true,
         },
       },
@@ -27,14 +26,13 @@ const TextCarousel: React.FC = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          autoplay: true,
           dots: true,
         },
       },
       {
         breakpoint: 1024, // Tamanho da tela para desktops
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
           dots: true,
         },
@@ -43,46 +41,49 @@ const TextCarousel: React.FC = () => {
   };
 
 
-
+  const sliderContainerStyle = {
+    margin: '0 50px', // Adicione a margem desejada aqui
+  };
 
   const slideStyle = {
     width: '100%',
-    margin: '15 auto',
+    margin: '0 auto',
     padding: '60px',
     borderRadius: '30px',
     background: '#f0f0f0',
-    marginRight: '10px'
+    marginRight: '100px'
   };
   
 
   
   return (
-    <div style={{ margin: '75px auto', maxWidth: '1024px' }}>
+    <div style={{ margin: '10px auto', maxWidth: '1024px' }}>
+         <div style={sliderContainerStyle}>
       <Slider {...settings}>
         <div>
           <div style={slideStyle}>
-            <h3>Slide 1: "Desde que comecei a terapia com a Dra. Talita, minha vida mudou para melhor. Sua abordagem atenciosa e carinhosa me ajudou a enfrentar minhas crises de ansiedade e a desenvolver técnicas para lidar com elas. Estou muito grata por todo o apoio e orientação que tenho recebido.” C.P.L 31 anos</h3>    
+            <h3>Slide 1: "Desde que comecei a terapia com a Dra. Talita, minha vida mudou para melhor. Sua abordagem atenciosa e carinhosa me ajudou a enfrentar minhas crises de ansiedade e a desenvolver técnicas para lidar com elas. Estou muito grata por todo o apoio e orientação que tenho recebido.” C.P.L 31 anos</h3>
           </div>
         </div>
         <div>
           <div style={slideStyle}>
-            <h3>Slide 2: “Depois de ter passado por alguns profissionais, ainda não tinha entendido como a terapia é um processo transformador.  Com a Talita consegui construir uma conexão e uma relação de confiança que foram de extrema importância para mim! Tem sido uma jornada de muito autoconhecimento e aprendizado, e, desde o início, me chama a atenção o quanto ela se importa e me faz sentir acolhido. Hoje, desejo que todos tenham uma Talita para que, assim como eu, se sintam encorajados a prosseguir!” D.M. 28 anos</h3>          
+            <h3>Slide 2: “Depois de ter passado por alguns profissionais, ainda não tinha entendido como a terapia é um processo transformador.  Com a Talita consegui construir uma conexão e uma relação de confiança que foram de extrema importância para mim! Tem sido uma jornada de muito autoconhecimento e aprendizado, e, desde o início, me chama a atenção o quanto ela se importa e me faz sentir acolhido. Hoje, desejo que todos tenham uma Talita para que, assim como eu, se sintam encorajados a prosseguir!” D.M. 28 anos</h3>
           </div>
         </div>
         <div>
           <div style={slideStyle}>
             <h3>Slide 3: "Profissional extremamente comprometida e atenciosa. Suas orientações e apoio foram cruciais para que eu conseguisse mudar minha vida. Sou grato por todo o suporte e dedicação que ela demonstrou em cada sessão. Recomendo de olhos fechados a todos que buscam uma mudança positiva em suas vidas." C.H.C. 33 anos</h3>
-          </div>          
+          </div>
         </div>
         <div>
           <div style={slideStyle}>
-            <h3>Slide 4: "Uma bondade de pessoa e uma excelente profissional. Sua competência e dedicação são evidentes em cada sessão. Ela se preocupa genuinamente com o desenvolvimento pessoal de seus pacientes, sabe ouvir e busca sempre oferecer o melhor suporte possível.” V.L. 39 anos</h3>          
+            <h3>Slide 4: "Uma bondade de pessoa e uma excelente profissional. Sua competência e dedicação são evidentes em cada sessão. Ela se preocupa genuinamente com o desenvolvimento pessoal de seus pacientes, sabe ouvir e busca sempre oferecer o melhor suporte possível.” V.L. 39 anos</h3>
           </div>
         </div>
         <div>
           <div style={slideStyle}>
             <h3>Depoimento 5: "A terapia com a Dra. Talita foi fundamental para meu autoconhecimento e crescimento pessoal. Descobri a origem de meus comportamentos e aprendi a expressar meus sentimentos com clareza. Suas sessões me proporcionaram um espaço seguro para desabafar e encontrar novas perspectivas.” A.C.S 27 anos"</h3>
-          </div>         
+          </div>
         </div>
       </Slider>
     </div>
