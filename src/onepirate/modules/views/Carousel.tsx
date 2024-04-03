d864b58a1d50a63e1b26003a2a9426cda87034f9
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-03 01:44:50 +0000

    .

--- src/onepirate/modules/views/Carousel.tsx
+++ src/onepirate/modules/views/Carousel.tsx
@@ -36,7 +36,7 @@ const TextCarousel: React.FC = () => {
           slidesToScroll: 1,
           dots: true,
         },
-      },
+      }, // Adicionei a marcação de fechamento aqui
     ],
   };
 
@@ -58,4 +58,27 @@ const TextCarousel: React.FC = () => {
         </div>
         <div>
           <div style={slideStyle}>
-            <h3>Slide 2: “Depois de ter passado por alguns profissionais, ainda não tinha entendido como a terapia é 
+            <h3>Slide 2: “Depois de ter passado por alguns profissionais, ainda não tinha entendido como a terapia é um processo transformador.  Com a Talita consegui construir uma conexão e uma relação de confiança que foram de extrema importância para mim! Tem sido uma jornada de muito autoconhecimento e aprendizado, e, desde o início, me chama a atenção o quanto ela se importa e me faz sentir acolhido. Hoje, desejo que todos tenham uma Talita para que, assim como eu, se sintam encorajados a prosseguir!” D.M. 28 anos</h3>
+          </div>
+        </div>
+        <div>
+          <div style={slideStyle}>
+            <h3>Slide 3: "Profissional extremamente comprometida e atenciosa. Suas orientações e apoio foram cruciais para que eu conseguisse mudar minha vida. Sou grato por todo o suporte e dedicação que ela demonstrou em cada sessão. Recomendo de olhos fechados a todos que buscam uma mudança positiva em suas vidas." C.H.C. 33 anos</h3>
+          </div>
+        </div>
+        <div>
+          <div style={slideStyle}>
+            <h3>Slide 4: "Uma bondade de pessoa e uma excelente profissional. Sua competência e dedicação são evidentes em cada sessão. Ela se preocupa genuinamente com o desenvolvimento pessoal de seus pacientes, sabe ouvir e busca sempre oferecer o melhor suporte possível.” V.L. 39 anos</h3>
+          </div>
+        </div>
+        <div>
+          <div style={slideStyle}>
+            <h3>Depoimento 5: "A terapia com a Dra. Talita foi fundamental para meu autoconhecimento e crescimento pessoal. Descobri a origem de meus comportamentos e aprendi a expressar meus sentimentos com clareza. Suas sessões me proporcionaram um espaço seguro para desabafar e encontrar novas perspectivas.” A.C.S 27 anos"</h3>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default TextCarousel;
