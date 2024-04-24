7f49ae539cce0dc53c853136a50444d1a6b551db
laramartinsa <166570180+laramartinsa@users.noreply.github.com>
2024-04-24 19:43:53 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,15 @@ import ReactGA from "react-ga4";
 function App() {
   React.useEffect(() => {
     // Inicialize o Pixel do Meta com seu ID
+    ReactGA.initialize('AW-16515472425');
+  }, []); // Garante que isso só seja executado uma vez
+
+
+  React.useEffect(() => {
+    // Inicialize o Pixel do Meta com seu ID
     ReactPixel.init('397619074247878');
   }, []); // Garante que isso só seja executado uma vez
-  
-  React.useEffect(() => {
-    ReactGA.initialize('AW-16515472425');
-  }, []);
+
 
   return (
     <React.Fragment>
