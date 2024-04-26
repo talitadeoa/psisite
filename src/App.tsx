075cbf05798183774e9294ce20b5b018a7d475f5
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2024-04-26 17:47:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,10 @@ import Resources from "./onepirate/Resources";
 import Groups from "./onepirate/Groups";
 import NotFound404 from "./onepirate/404";
 import Floating from "./onepirate/modules/components/Floating";
-import ReactPixel from 'react-facebook-pixel'; 
-import ReactGA from "react-ga4";
+
 
 function App() {
 
-  React.useEffect(() => {
-    ReactGA.initialize('AW-16515472425');
-  }, []);
-
-
-  React.useEffect(() => {
-    // Inicialize o Pixel do Meta com seu ID
-    ReactPixel.init('397619074247878');
-  }, []); // Garante que isso só seja executado uma vez
-  
 
   return (
     <React.Fragment>
